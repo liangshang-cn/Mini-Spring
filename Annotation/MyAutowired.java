a92ba4b92975b675b7b6713b742553151e6e2eb8
liangshang-cn <66558919+liangshang-cn@users.noreply.github.com>
2020-10-23 20:33:47 +0800

    Add files via upload

---
+++ Annotation/MyAutowired.java
@@ -0,0 +1,11 @@
+package Annotation;
+
+
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyAutowired {
+    String value() default "";
+}
---
+++ Annotation/MyController.java
@@ -0,0 +1,10 @@
+package Annotation;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyController {
+    String value() default "";
+}
